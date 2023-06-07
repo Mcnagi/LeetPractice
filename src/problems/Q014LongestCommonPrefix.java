00fb635980fb6c5350338c9b637b346f0a021f89
McNagi <huanjie@protonmail.com>
2023-06-08 04:55:43 +1000

    Implement q14 lcp

---
+++ src/problems/Q014LongestCommonPrefix.java
@@ -0,0 +1,25 @@
+package problems;
+
+public class Q014LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 1) return strs[0];
+        StringBuilder builder = new StringBuilder();
+        int i = 0;
+        boolean good = true;
+        while (good && i < strs[0].length()) {
+            good = false;
+            Character c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (i >= strs[j].length() || c != strs[j].charAt(i)) {
+                    good = false;
+                    break;
+                }
+                good = true;
+            }
+            if (!good) break;
+            builder.append(c);
+            i++;
+        }
+        return builder.toString();
+    }
+}
