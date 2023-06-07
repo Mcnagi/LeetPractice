043ecea07430538b13cf05ccc46406db35721103
McNagi <huanjie@protonmail.com>
2023-06-08 04:42:46 +1000

    Implement q13 romanToInteger

---
+++ src/problems/Q013RomanToInteger.java
@@ -0,0 +1,47 @@
+package problems;
+
+public class Q013RomanToInteger {
+    /**
+     * I             1
+     * V             5
+     * X             10
+     * L             50
+     * C             100
+     * D             500
+     * M             1000
+     */
+    public enum Roman{
+        I (1), V (5),
+        X (10), L (50),
+        C(100),D(500),
+        M(1000);
+        int value;
+        Roman(int value) {
+            this.value = value;
+        }
+    }
+    private Roman getRoman(Character c) {
+        return switch (c) {
+            case 'I' -> Roman.I;
+            case 'V' -> Roman.V;
+            case 'X' -> Roman.X;
+            case 'L' -> Roman.L;
+            case 'C' -> Roman.C;
+            case 'D' -> Roman.D;
+            case 'M' -> Roman.M;
+            default -> null;
+        };
+    }
+    public int romanToInt(String s) {
+        Roman prevRoman = getRoman(s.charAt(0));
+        int result = 0;
+        for (int i = 1; i < s.length(); i++) {
+             Roman currentRoman = getRoman(s.charAt(i));
+            if (prevRoman.value >= currentRoman.value) result += prevRoman.value;
+            else result -= prevRoman.value;
+            prevRoman = currentRoman;
+        }
+        result += prevRoman.value;
+        return result;
+    }
+}
