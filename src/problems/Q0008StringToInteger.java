49c31d56bf8fe62d67b3dac572a1fbdcf280a97a
McNagi <huanjie@protonmail.com>
2023-06-09 12:17:37 +1000

    minor changes

---
+++ src/problems/Q0008StringToInteger.java
@@ -0,0 +1,4 @@
+package problems;
+
+public class Q0008StringToInteger {
+}
--- test/problems/Q0008Test.java
+++ test/problems/Q0008Test.java
@@ -1,4 +1,14 @@
 package problems;
 
+import org.junit.jupiter.api.Test;
+
 public class Q0008Test {
+    public void test() {
+
+    }
+
+    @Test
+    public void testSimple() {
+
+    }
 }
