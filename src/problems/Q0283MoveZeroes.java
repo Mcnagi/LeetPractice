5c480b0f34c347bcd12adc0af2cb6b332caeffb1
McNagi <huanjie@protonmail.com>
2023-06-10 19:57:54 +1000

    Implement q27 and 283 in Leetcode and pass
    Easy questions and hence no test cases.

---
+++ src/problems/Q0283MoveZeroes.java
@@ -0,0 +1,19 @@
+package problems;
+
+public class Q0283MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int size = 0;
+        int countZeros = 0;
+        for (int i = 0; size + countZeros < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[size] = nums[i];
+                size += 1;
+            } else {
+                countZeros += 1;
+            }
+        }
+        for (int i = 0; i < countZeros; i++) {
+            nums[size + i] = 0;
+        }
+    }
+}
