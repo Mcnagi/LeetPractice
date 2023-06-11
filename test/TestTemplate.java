01d32bbb07994ea71d77196d46b4af7648d84128
McNagi <huanjie@protonmail.com>
2023-06-11 10:36:41 +1000

    Implement q11 with O(n^2)

--- test/TestTemplate.java
+++ test/TestTemplate.java
@@ -4,23 +4,23 @@
 //public class TestTemplate {
 //
 //    public void test(int expected, int input) {
-//        var q = new q();
-//        Assertions.assertEquals(expected, q.solution(),
-//                String.format("Expect to have %s with input: %s", expected, input));
-//    }
-//
-//    @Test
-//    public void testSimple() {
-//
-//    }
-//
-//    @Test
-//    public void testMedian() {
-//
-//    }
-//
-//    @Test
-//    public void testHard() {
-//
-//    }
+////        var q = new q();
+////        Assertions.assertEquals(expected, q.solution(),
+////                String.format("Expect to have %s with input: %s", expected, input));
+////    }
+////
+////    @Test
+////    public void testSimple() {
+////
+////    }
+////
+////    @Test
+////    public void testMedian() {
+////
+////    }
+////
+////    @Test
+////    public void testHard() {
+////
+////    }
 //}
