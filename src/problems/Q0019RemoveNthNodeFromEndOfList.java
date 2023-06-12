658084f4c22faef1c5752e9e358388f3b21480d3
McNagi <huanjie@protonmail.com>
2023-06-12 17:31:54 +1000

    Implement Q19 Remove Nth Node from End of List

---
+++ src/problems/Q0019RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,45 @@
+package problems;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q0019RemoveNthNodeFromEndOfList {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    // sacrifice memory for time.
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        List<ListNode> list = new ArrayList<>();
+        ListNode current = head;
+        while (current != null) {
+            list.add(current);
+            current = current.next;
+        }
+        int rmIndex = list.size() - n;
+        if (rmIndex == 0) {
+            if (list.size() == 1) return null;
+            else return head.next;
+        }
+        if (rmIndex == list.size() - 1) {
+            list.get(rmIndex - 1).next = null;
+        }
+        else list.get(rmIndex - 1).next = list.get(rmIndex + 1);
+        return head;
+    }
+
+}
