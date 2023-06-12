65e47e490c995ef826da2553f7b49060146b305f
McNagi <huanjie@protonmail.com>
2023-06-12 17:10:49 +1000

    Implement Q203 Remove Linked List Elements

---
+++ src/problems/Q0203RemoveLinkedListElements.java
@@ -0,0 +1,38 @@
+package problems;
+
+public class Q0203RemoveLinkedListElements {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {
+        }
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    class Solution {
+        public ListNode removeElements(ListNode head, int val) {
+            while (head != null && head.val == val) {
+                head = head.next;
+            }
+            ListNode current = head;
+            ListNode prev = null;
+            while (current != null) {
+                if (current.val == val) {
+                    prev.next = current.next;
+                    current = current.next;
+                    continue;
+                }
+                prev = current;
+                current = current.next;
+            }
+            return head;
+        }
+    }
+}
