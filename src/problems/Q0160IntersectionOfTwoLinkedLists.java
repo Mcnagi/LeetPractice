221ca3df571174bed5b2b2f92cc56126d6afc45d
McNagi <huanjie@protonmail.com>
2023-06-12 17:02:08 +1000

    Implement q160 intersection of two linked lists
    Runtime 3 ms Beats 28.13%.
    Memory 46.9 MB Beats 24.45%.

---
+++ src/problems/Q0160IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,41 @@
+package problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q0160IntersectionOfTwoLinkedLists {
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public class Solution {
+        public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+            List<ListNode> listA = new ArrayList<>();
+            List<ListNode> listB = new ArrayList<>();
+            ListNode current = headA;
+            while (current != null) {
+                listA.add(current);
+                current = current.next;
+            }
+            current = headB;
+            while (current != null) {
+                listB.add(current);
+                current = current.next;
+            }
+            int indexA = listA.size() - 1;
+            int indexB = listB.size() - 1;
+            while (indexA >=0 && indexB >= 0 && listA.get(indexA) == listB.get(indexB)) {
+                indexA --;
+                indexB --;
+            }
+            return indexA + 1 == listA.size() ? null : listA.get(indexA + 1);
+        }
+    }
+}
