e56177cf0cca300a7edbf4c0c4e8cc24228b8346
McNagi <huanjie@protonmail.com>
2023-06-13 23:18:00 +1000

    Implement q155 with array
    Faster than arraylist.

--- src/problems/Q0155MinStack.java
+++ src/problems/Q0155MinStack.java
@@ -5,31 +5,48 @@ import java.util.List;
 
 public class Q0155MinStack {
     class MinStack {
-        private List<Integer> stack;
-        private List<Integer> minStack;
+        private final int INITIAL_SIZE = 32;
+        private final int GROWTH_FACTOR = 2;
+        private Integer[] array;
+        private Integer[] minStack;
+        private int size;
+
         public MinStack() {
-            this.stack = new ArrayList<>();
-            this.minStack = new ArrayList<>();
-            minStack.add(Integer.MAX_VALUE);
+            array = new Integer[INITIAL_SIZE];
+            minStack = new Integer[INITIAL_SIZE];
+            size = 0;
+            minStack[0] = Integer.MAX_VALUE;
         }
 
         public void push(int val) {
-            this.stack.add(val);
-            if (val < minStack.get(minStack.size() - 1)) minStack.add(val);
-            else minStack.add(minStack.get(minStack.size() - 1));
+            if (size == array.length) {
+                Integer[] tmp = new Integer[array.length * GROWTH_FACTOR];
+                System.arraycopy(array, 0, tmp, 0, array.length);
+                array = tmp;
+            }
+            if (size + 1 == minStack.length) {
+                Integer[] tmp = new Integer[minStack.length * GROWTH_FACTOR];
+                System.arraycopy(minStack, 0, tmp, 0, minStack.length);
+                minStack = tmp;
+            }
+            array[size] = val;
+            if (val < minStack[size]) minStack[size + 1] = val;
+            else minStack[size + 1] = minStack[size];
+            size += 1;
         }
 
         public void pop() {
-            stack.remove(stack.size() - 1);
-            minStack.remove(minStack.size() - 1);
+            array[size - 1] = null;
+            minStack[size] = null;
+            size -= 1;
         }
 
         public int top() {
-            return stack.get(stack.size() - 1);
+            return array[size - 1];
         }
 
         public int getMin() {
-            return minStack.get(minStack.size() - 1);
+            return minStack[size];
         }
     }
 
