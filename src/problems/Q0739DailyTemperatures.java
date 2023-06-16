8404980e620e9d6dd1a38b863e861b032f38db92
McNagi <huanjie@protonmail.com>
2023-06-16 22:09:14 +1000

    Implement q739 backward fill

---
+++ src/problems/Q0739DailyTemperatures.java
@@ -0,0 +1,25 @@
+package problems;
+
+public class Q0739DailyTemperatures {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+        result[result.length - 1] = 0;
+        if (result.length == 1) return result;
+        for (int i = temperatures.length - 2; i >= 0; i--) {
+            if (temperatures[i] < temperatures[i + 1]) result[i] = 1;
+            else {
+                int j = i + result[i + 1];
+                while (j < result.length) {
+                    if (temperatures[i] < temperatures[j]) {
+                        result[i] = j - i;
+                        break;
+                    }
+                    j ++;
+                }
+            }
+        }
+        return result;
+    }
+
+
+}
