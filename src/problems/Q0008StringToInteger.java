d56ecac1a40a3fea12bae12bd354b7e0dc3a58f5
McNagi <huanjie@protonmail.com>
2023-07-03 07:56:31 +1000

    Implement q8 atoi

--- src/problems/Q0008StringToInteger.java
+++ src/problems/Q0008StringToInteger.java
@@ -3,18 +3,30 @@ package problems;
 public class Q0008StringToInteger {
     public int myAtoi(String s) {
         boolean positive = true;
-        int result = 0;
+        long result = 0;
         int index = 0;
         while (index < s.length() && s.charAt(index) == ' ') index += 1;
-        if (index < s.length() && s.charAt(index) == '-') {
-            positive = false;
-            index += 1;
+        if (index < s.length()) {
+            char c = s.charAt(index);
+            if (c == '-') {
+                positive = false;
+                index += 1;
+            } else if (c == '+') index += 1;
         }
-        while (index < s.length() && Character.isDigit(s.charAt(index))) {
-            result = (result * 10) + s.charAt(index) - '0';
-            index += 1;
+        if (positive) {
+            while (index < s.length() && Character.isDigit(s.charAt(index))) {
+                result = (result * 10) + s.charAt(index) - '0';
+                if (result >= Integer.MAX_VALUE) return Integer.MAX_VALUE;
+                index += 1;
+            }
+        } else {
+            while (index < s.length() && Character.isDigit(s.charAt(index))) {
+                result = (result * 10) - s.charAt(index) + '0';
+                if (result <= Integer.MIN_VALUE) return Integer.MIN_VALUE;
+                index += 1;
+            }
         }
-
-        return result * (positive ? 1 : -1);
+        return (int) result;
     }
+
 }
