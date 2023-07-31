d79706963af3fc3207785f7b34331f84e887933d
Mcnagi <huanjie@protonmail.com>
2023-07-31 11:55:06 +1000

    update q23 from leetcode

---
+++ src/problems/Q0023MergeKSortedLists.java
@@ -0,0 +1,26 @@
+package problems;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Q0023MergeKSortedLists {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) return null;
+        List<ListNode> array = new ArrayList<>();
+        for (int i = 0; i < lists.length; i++) {
+            ListNode current = lists[i];
+            while (current != null) {
+                array.add(current);
+                current = current.next;
+            }
+        }
+        if (array.size() == 0) return null;
+        array.sort(Comparator.comparingInt(x -> x.val));
+        for (int i = 0; i < array.size() - 1; i++) {
+            array.get(i).next = array.get(i + 1);
+        }
+        return array.get(0);
+    }
+}
