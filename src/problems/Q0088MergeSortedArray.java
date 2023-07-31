575fd0204ce013e8c6e8c3cedcdcbfe0d55fc3e4
McNagi <huanjie@protonmail.com>
2023-07-31 15:36:16 +1000

    Implement q88 and pass

---
+++ src/problems/Q0088MergeSortedArray.java
@@ -0,0 +1,26 @@
+package problems;
+
+public class Q0088MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (n == 0) return;
+        else if (m == 0) {
+            System.arraycopy(nums2, 0, nums1, 0, n);
+            return;
+        }
+
+        int[] copied = new int[m];
+        System.arraycopy(nums1, 0, copied, 0, m);
+
+        int size = 0, i = 0, j = 0, tmp = 0;
+        while (i < m || j < n) {
+            if (j == n || (i < m && copied[i] < nums2[j])) {
+                nums1[size] = copied[i];
+                i++;
+            } else {
+                nums1[size] = nums2[j];
+                j++;
+            }
+            size++;
+        }
+    }
+}
