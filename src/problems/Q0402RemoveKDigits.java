0a8cfad38a02517edfb60d8fcd307666232b9805
McNagi <huanjie@protonmail.com>
2023-08-01 21:28:15 +1000

    add test for q402
    string operation is nightmare!

---
+++ src/problems/Q0402RemoveKDigits.java
@@ -0,0 +1,40 @@
+package problems;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+public class Q0402RemoveKDigits {
+
+//    Integer[] indices;
+//    int size = 0;
+//    public String removeKdigits(String num, int k) {
+//        if (num.length() == k) return "0";
+//
+//        indices = new Integer[num.length() - k];
+//
+//        for (int i = 0; i < num.length(); i++) {
+//
+//        }
+//
+//        StringBuilder builder = new StringBuilder();
+//        for (Integer index : indices) {
+//            if (index == null) break;
+//            builder.append(index);
+//        }
+//        return builder.isEmpty() ? "0" : builder.toString();
+//    }
+//    void add(int value) {
+//        indices[size ++] = value;
+//    }
+//    int getLastIndex() {
+//        return indices[size - 1];
+//    }
+//    void replaceAt(int value, int index) {
+//        indices[index] = value;
+//    }
+//    void replaceLast(int value) {
+//        indices[size - 1] = value;
+//    }
+}
--- src/problems/playground.java
+++ src/problems/playground.java
@@ -5,9 +5,13 @@ import java.util.List;
 
 public class playground {
     public static void main(String[] args) {
-        List<Integer> tmp = new ArrayList<>(8);
-        System.out.println(tmp.size());
-        tmp.add(10);
-        System.out.println(tmp.size());
+//        List<Integer> tmp = new ArrayList<>(8);
+//        System.out.println(tmp.size());
+//        tmp.add(10);
+//        System.out.println(tmp.size());
+        Character c = '1';
+//        System.out.println(c > null);
+        char[] chars = new char[10];
+        System.out.println();
     }
 }
