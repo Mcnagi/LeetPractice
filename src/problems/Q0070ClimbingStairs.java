9ea22f72af1c858b15d9b9f53e452b91b2101377
Huanjie Zhang <u7633338@anu.edu.au>
2023-08-20 16:59:52 +1000

    implement q70 fibonacci
    Used formula.

---
+++ src/problems/Q0070ClimbingStairs.java
@@ -0,0 +1,23 @@
+package problems;
+
+public class Q0070ClimbingStairs {
+    static int[] results = new int[46];
+    static {
+        results[0] = 0;
+        results[1] = 1;
+        results[2] = 2;
+        results[3] = 3;
+        results[4] = 5;
+    }
+    public int climbStairs(int n) {
+        double alpha = (Math.sqrt(5) + 1) / 2;
+        return (int)((Math.pow(alpha, n) - Math.pow(1-alpha, n)) / Math.sqrt(5));
+    }
+
+    public static void main(String[] args) {
+        var q = new Q0070ClimbingStairs();
+        for (int i = 1; i < 10; i++) {
+            System.out.println("i: " + i +", " + q.climbStairs(i + 1));
+        }
+    }
+}
