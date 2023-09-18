1295fdf1bae1908b39a3e35f2e3c1f47eca28e76
McNagi <huanjie@protonmail.com>
2023-09-18 15:10:11 +1000

    implement q34 find first last

--- src/problems/Q0034FindFirstLast.java
+++ src/problems/Q0034FindFirstLast.java
@@ -1,51 +1,49 @@
 package problems;
 
-public class Q0034FindFirstLast {
-    public int[] searchRange(int[] nums, int target) {
-        if (nums.length == 0) return new int[]{-1, -1};
-        int s = 0, e = nums.length - 1;
-        int k = (s + e ) / 2;
-        while (s < e) {
-            if (nums[k] == target) break;
-            if (nums[k] < target) s = k + 1;
-            else e = k - 1;
-            k = (s + e ) / 2;
-        }
-        if (nums[k] != target) return new int[]{-1, -1};
-        int left = k, right = k;
-        s = 0;
-        e = left;
-        k = (s + e) / 2;
-        while (s < e) {
-            if (k == 0) {
-                if (nums[0] == target) left = 0;
-                else left = 1;
-                break;
-            }
-            if (nums[k] == target && nums[k - 1] < target) {
-                left = k;
-                break;
-            }
-            if (nums[k] < target) s = k + 1;
-            else e = k - 1;
-            k = (s + e) / 2;
-        }
-        s = right;
-        e = nums.length;
-        k = (s + e) / 2;
-        while (s < e) {
-            if (k == nums.length - 1) {
-             if (nums[k] == target) right = k;
-             else right = k - 1;
-            }
-            if (nums[k] == target && nums[k + 1] > target) {
-                right = k;
-                break;
-            }
-            if (nums[k] < target) s = k + 1;
-            else e = k - 1;
-        }
-        return new int[]{left, right};
-    }
+import java.util.function.Predicate;
 
+public class Q0034FindFirstLast {
+  int[] array;
+  int target;
+
+  public int[] searchRange(int[] nums, int target) {
+    if (nums.length == 0) return new int[] {-1, -1};
+
+    this.array = nums;
+    this.target = target;
+    int k = binarySearch(0, array.length - 1, x -> array[x] == target, x -> array[x] < target);
+    if (k == -1) return new int[] {-1, -1};
+
+    int left =
+        binarySearch(
+            0,
+            k,
+            x -> array[x] == target && (x == 0 || array[x - 1] < target),
+            x -> array[x] < target);
+    if (array[left] < target) left += 1;
+
+    int right =
+        binarySearch(
+            k,
+            array.length - 1,
+            x -> array[x] == target && (x == array.length - 1 || array[x + 1] > target),
+            x -> array[x] == target);
+    if (array[right] > target) right -= 1;
+
+    return new int[] {left, right};
+  }
+
+  private int binarySearch(int s, int e, Predicate<Integer> pred, Predicate<Integer> pred2) {
+    int i = (s + e) / 2;
+    while (s < e) {
+      if (pred.test(i)) {
+        return i;
+      }
+      if (pred2.test(i)) s = i + 1;
+      else e = i - 1;
+      i = (s + e) / 2;
+    }
+    if (pred.test(i)) return i;
+    return -1;
+  }
 }
