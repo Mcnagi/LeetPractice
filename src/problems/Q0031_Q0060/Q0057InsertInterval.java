fc8c0778fdf51d0c47de6da107fcbcd767afb4ed
McNagi <huanjie@protonmail.com>
2023-12-29 00:13:20 +1100

    q57 insert interval
    implement the method and test for binary search.

--- src/problems/Q0031_Q0060/Q0057InsertInterval.java
+++ src/problems/Q0031_Q0060/Q0057InsertInterval.java
@@ -1,25 +1,59 @@
 package problems.Q0031_Q0060;
 
+import java.util.LinkedList;
+
 public class Q0057InsertInterval {
 
+    // maybe no need for binary search, but can be done looping through inputs linearly.
+
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        return new int[0][0];
+        if (intervals.length == 0) return new int[][]{newInterval};
+
+        int index = binarySearch(intervals, newInterval[0]);
+        LinkedList<int[]> linkedList = new LinkedList<>();
+        int i = 0;
+        while (i < intervals.length) {
+            if (i == index) addInterval(linkedList, newInterval);
+            addInterval(linkedList, intervals[i]);
+            i ++;
+        }
+        if (i == index) addInterval(linkedList, newInterval);
+        int[][] output = new int[linkedList.size()][2];
+        i = 0;
+        while (linkedList.size() > 0) {
+            output[i ++] = linkedList.pollFirst();
+        }
+        return output;
     }
 
     /**
      * finds the index in the source, that
      * first element is no greater than target.
      */
-    private int binarySearch(int[][] source, int target) {
-        if (source.length == 0) return -1;
+    int binarySearch(int[][] source, int target) {
+        if (source.length == 0) return 0;
         int s = 0, e = source.length - 1;
         while(s < e) {
             int i = (s + e) / 2;
             int comp = Integer.compare(source[i][0], target);
             if(comp == 0) return i;
-            else if (comp < 0) e = i - 1;
+            else if (comp > 0) e = i - 1;
             else s = i + 1;
         }
+        if (s == source.length - 1 && target > source[s][0]) return s + 1;
+        if (source[s][0] < target && target < source[s + 1][0]) return s + 1;
         return s;
+    }
+
+    private void addInterval(LinkedList<int[]> list, int[] newInterval) {
+        if (list.size() == 0) {
+            list.add(newInterval);
+        }
+        else if (list.peekLast()[1] >= newInterval[0]) {
+            list.peekLast()[1] = Math.max(list.peekLast()[1], newInterval[1]);
+        }
+        else {
+            list.add(newInterval);
+        }
     }
 }
---
+++ src/problems/Q0031_Q0060/Q0057TestBinarySearch.java
@@ -0,0 +1,63 @@
+package problems.Q0031_Q0060;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+
+public class Q0057TestBinarySearch {
+    static Q0057InsertInterval q;
+    @BeforeAll
+    public static void init() {
+        q = new Q0057InsertInterval();
+    }
+
+    @Test
+    public void testEmpty() {
+        int[][] input = new int[0][0];
+        Assertions.assertEquals(0, q.binarySearch(input, 1));
+        Assertions.assertEquals(0, q.binarySearch(input, 2));
+        Assertions.assertEquals(0, q.binarySearch(input, -1));
+        Assertions.assertEquals(0, q.binarySearch(input, 0));
+    }
+
+    @Test
+    public void testOne() {
+        int[][] input = new int[][] {new int[] {1,5}};
+        Assertions.assertEquals(0, q.binarySearch(input, 0));
+        Assertions.assertEquals(0, q.binarySearch(input, -1));
+        Assertions.assertEquals(0, q.binarySearch(input, 1));
+        Assertions.assertEquals(1, q.binarySearch(input, 2));
+        Assertions.assertEquals(1, q.binarySearch(input, 5));
+        Assertions.assertEquals(1, q.binarySearch(input, 10));
+
+        input = new int[][] {new int[] {1,1}};
+        Assertions.assertEquals(0, q.binarySearch(input, 1));
+        Assertions.assertEquals(0, q.binarySearch(input, -1));
+        Assertions.assertEquals(1, q.binarySearch(input, 10));
+    }
+
+    @Test
+    public void testComplex() {
+        int[][] input = new int[][] {new int[] {1,5}, {3, 5}, {6, 6}};
+        Assertions.assertEquals(0, q.binarySearch(input, -1));
+        Assertions.assertEquals(0, q.binarySearch(input, 0));
+        Assertions.assertEquals(0, q.binarySearch(input, 1));
+        Assertions.assertEquals(1, q.binarySearch(input, 2));
+        Assertions.assertEquals(1, q.binarySearch(input, 3));
+        Assertions.assertEquals(2, q.binarySearch(input, 4));
+        Assertions.assertEquals(2, q.binarySearch(input, 5));
+        Assertions.assertEquals(2, q.binarySearch(input, 6));
+        Assertions.assertEquals(3, q.binarySearch(input, 7));
+    }
+
+    @Test
+    public void testLong() {
+        int[][] input = new int[][] {new int[] {1,2},
+                new int[] {3,5},
+                new int[] {6,7},
+                new int[] {8,10},
+                new int[] {12,16}};
+        Assertions.assertEquals(2, q.binarySearch(input, 4));
+    }
+}
